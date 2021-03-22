0464e7e99b896af19f24106fe5757936930c6790
jankrejci77 <jankrejci77@gmail.com>
2021-03-22 20:17:45 +0100

    Train departure animation clip

---
+++ Pejsek/Assets/Scripts/IntroductionVideo/SoundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    [Header("AudioSource")]
+    [SerializeField] AudioSource audioSrc = default;
+    
+    [Header("Sounds")]
+    [SerializeField] AudioClip trainBraking = default;
+    [SerializeField] AudioClip trainDepart = default;
+    [SerializeField] AudioClip trainDoorOpening = default;
+    [SerializeField] AudioClip trainPassingBy = default;
+
+
+    internal void PlayAudio(string audioClip) {
+        switch (audioClip) {
+            case "trainBraking":
+                audioSrc.PlayOneShot(trainBraking);
+                break;
+
+            case "trainDepart":
+                audioSrc.PlayOneShot(trainDepart);
+                break;
+
+            case "trainDoorOpening":
+                audioSrc.PlayOneShot(trainDoorOpening);
+                break;
+
+            case "trainPassingBy":
+                audioSrc.PlayOneShot(trainPassingBy);
+                break;
+        }
+    }
+
+    internal void StopAudio() {
+        audioSrc.Stop();
+    }
+}
---
+++ Pejsek/Assets/Scripts/IntroductionVideo/VideoManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoManager : MonoBehaviour
+{
+    [SerializeField] SoundManager soundManager = default;
+
+    [SerializeField] Animator latecomerAnimator = default;
+
+    // Start is called before the first frame update
+    void Start() {
+        soundManager.PlayAudio("trainDepart");
+    }
+
+    // Load next introduction scene
+    void NextScene() {
+        soundManager.StopAudio();
+
+    }
+
+
+    // Play sounds, called by animation events
+    void OpenDoor() {
+        soundManager.PlayAudio("trainDoorOpening");
+    }
+
+    void StartBraking() {
+        soundManager.PlayAudio("trainBraking");
+    }
+
+    void LatecomerStartRun() {
+        latecomerAnimator.SetTrigger("Start");
+    }
+
+
+}
